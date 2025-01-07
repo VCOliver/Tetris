c7ecb7a27748f6f92e5d566a1fcf5ddb811de194
VCOliver <victorcruz.01@gmail.com>
2025-01-06 22:21:21 -0300

    Added colors most likely to be used

--- include/utils/colors.hpp
+++ include/utils/colors.hpp
@@ -25,4 +25,10 @@ namespace Colors
     constexpr Color RED(255, 0, 0);
     constexpr Color GREEN(0, 255, 0);
     constexpr Color BLUE(0, 0, 255);
+    constexpr Color WHITE(255, 255, 255);
+    constexpr Color BLACK(0, 0, 0);
+    constexpr Color CYAN(0, 255, 255);
+    constexpr Color YELLOW(255, 255, 0);
+    constexpr Color PURPLE(128, 0, 128);
+    constexpr Color ORANGE(255, 165, 0);
 }
